d8cf6b3c3322743937e861bacf10beb97d3906be
NnopponS <105480619+NnopponS@users.noreply.github.com>
2022-07-13 23:21:45 +0700

    My Program ^^

---
+++ Lecture97_SQLite_HW/Lecture97_SQLite_HW/DataAccess.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture97_SQLite_HW
+{
+    class DataAccess
+    {
+        //Initialize the SQLite database
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS CustomersTable (userID INTEGER PRIMARY KEY, " +
+                    "firstName NVARCHAR(20) NULL, " +
+                    "lastName NVARCHAR(20) NULL, " +
+                    "EMail NVARCHAR(20) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        //Insert
+        public static void AddData(string inputfirstName, string inputlastName, string inputemail)
+        {
+            using (SqliteConnection db =
+              new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO CustomersTable VALUES (Null, @firstName, @lastName, @email);";
+                insertCommand.Parameters.AddWithValue("@firstName", inputfirstName);
+                insertCommand.Parameters.AddWithValue("@lastName", inputlastName);
+                insertCommand.Parameters.AddWithValue("@email", inputemail);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+
+        //Retrieve data from the SQLite database
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT firstName, lastName, email from CustomersTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                    entries.Add(query.GetString(1));
+                    entries.Add(query.GetString(2));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
---
+++ Lecture97_SQLite_HW/Lecture97_SQLite_HW/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture97_SQLite_HW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+        }
+
+        private void saveBT_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(firstName_TxT.Text, lastName_TxT.Text, Email_TxT.Text);
+            firstName_TxT.Text = "First Name";
+            lastName_TxT.Text = "Last Name";
+            Email_TxT.Text = "Email";
+            MessageBox.Show("Saved successfully");
+
+        }
+
+        private void ShowBT_Click(object sender, RoutedEventArgs e)
+        {
+            string Showdata = "";
+            foreach (string i in DataAccess.GetData())
+            {
+                Showdata = Showdata + i + "\n";
+            }
+            MessageBox.Show(Showdata);
+
+
+        }
+
+        private void firstName_TxT_GotFocus(object sender, RoutedEventArgs e)
+        {
+            firstName_TxT.Text = "";
+        }
+
+        private void lastName_TxT_GotFocus(object sender, RoutedEventArgs e)
+        {
+            lastName_TxT.Text = "";
+        }
+
+        private void Email_TxT_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Email_TxT.Text = "";
+        }
+    }
+}
